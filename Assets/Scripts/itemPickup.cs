8f512854093480efec33b29826f797ff5304c06a
Roberbot <42631146+Roberbot@users.noreply.github.com>
2019-01-20 00:13:45 +0100

    Animasjoner og itemPickup

---
+++ Assets/Scripts/itemPickup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class itemPickup : MonoBehaviour {
+	
+	public int itemType;
+	public int itemAmount;
+    
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.name == "PlayerController") {
+
+			Destroy(gameObject);
+		}
+	}
+}
